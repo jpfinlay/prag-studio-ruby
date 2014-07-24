4d8cd141d9e4308dcf97a11afbed0eb601228468
Jamie Finlay <jpfinlay@gmail.com>
2014-07-24 22:14:29 +0100

    Completed chapter 3 - numbers and strings

---
+++ fundraiser/crowfund.rb
@@ -0,0 +1,12 @@
+project1_name = "Crohn's cure"
+project1_funding = 1000000
+
+project2_name = "Ultralight bicycle mudguards"
+project2_funding = 5001
+
+project3_name = "Medication checker app"
+project3_funding = 3000
+
+puts "Project #{project1_name} has $#{project1_funding} in funding"
+
+puts "Projects:\n\t#{project1_name}\n\t#{project2_name}\n\t#{project3_name}"
