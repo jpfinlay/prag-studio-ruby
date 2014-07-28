e8aaf88f9132fee3ba054e331d76c00ebb8bd534
Jamie Finlay <jpfinlay@gmail.com>
2014-07-28 10:52:04 +0100

    Refactored project spec file to use before block

--- fundraiser/project_spec.rb
+++ fundraiser/project_spec.rb
@@ -1,32 +1,32 @@
 require_relative 'project'
 
 describe Project do
-  
+  before(:each) do
+    @initial_funding = 100
+    @target = 1000
+    @project = Project.new("new project", @initial_funding, @target)
+  end
+
   it "has a title in BLOCK CAPITALS" do
-    p = Project.new("a new project",10,100)
-    expect(p.name).to eq("A NEW PROJECT")
+    expect(@project.name).to eq("NEW PROJECT")
   end
   
   it "adds 25 to the funding total" do
-    p = Project.new("a new project",10,100)
-    p.add_funds
-    expect(p.funding).to eq(10 + 25)
+    @project.add_funds
+    expect(@project.funding).to eq(100 + 25)
   end
 
   it "subtracts 15 from the funding total" do
-    p = Project.new("a new project",10,100)
-    p.remove_funds
-    expect(p.funding).to eq(10 - 15)
+    @project.remove_funds
+    expect(@project.funding).to eq(100 - 15)
   end
 
   it "calcuates the funding needed by subtracting the current funding from the target funding" do
-    p = Project.new("a new project",10,100)
-    p.funding_needed
-    expect(p.funding_needed).to eq(100 - 10)
+    @project.funding_needed
+    expect(@project.funding_needed).to eq(1000 - 100)
   end
 
   it "has a string representation" do
-    p = Project.new("a new project",10,100)
-    expect(p.to_s).to eq("Project #{p.name} has $#{p.funding} in funding towards a goal of $#{p.target}.\nFunding required: #{p.funding_needed}.")
+    expect(@project.to_s).to eq("Project #{@project.name} has $#{@project.funding} in funding towards a goal of $#{@project.target}.\nFunding required: #{@project.funding_needed}.")
   end
 end
